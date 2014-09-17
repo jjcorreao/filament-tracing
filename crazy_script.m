d07996fc7110bebc64f04b608bca5d946b0d0278
jjcorreao <JoaquinCorrea@lbl.gov>
2014-09-17 15:55:42 -0700

    First commit

---
+++ crazy_script.m
@@ -0,0 +1,21 @@
+% 
+% crazy_script.m
+% This is under the works
+% 
+% Choi Y.1, Hassan A.1, Correa J.2
+% Lawrence Berkeley National Laboratory
+% 1 Life Sciences Division (LSD)
+% 2 National Energy Research Scientific Computing (NERSC)
+% 
+% Description:
+% Generate a intensity map based on normalized pixel data
+% CSV headers are
+% Pixel Width	Start Pixel	End Pixel	Avg Intensity	Max Intensity	Pixel of Max Intensity
+% 
+% vars
+% px        Pixel Width
+% avgi      Avg Intensity
+% maxi      Max Intensity
+% pmaxi     Pixel of Max Intensity
+% 
+% 
